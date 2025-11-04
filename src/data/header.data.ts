e85717b98e7cdd700751a9e077f3580b23cd5521
8023time <2162105974@qq.com>
2025-11-04 15:37:57 +0800

    commit

--- src/data/header.data.ts
+++ src/data/header.data.ts
@@ -8,8 +8,6 @@ export const MAIN_NAV_CONFIG: {
   logo?: string | HTMLElement;
 }[] = [
   { name: "首页", href: "/" },
-  { name: "前端", href: "/about" },
-  { name: "后端", href: "/about" },
   { name: "技能", href: "/skill" },
   { name: "留言区", href: "/article" },
   { name: "网站合集", href: "/site" },
