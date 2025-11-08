76b3a188ab3c9f4d2a0f9f178d7d9eb36770b85f
8023time <2162105974@qq.com>
2025-11-08 17:03:27 +0800

    📦️ build: 取消鼠标的动态效果

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import router from '@router/index';
-import Cursor from '@components/Cursor';
+// import Cursor from '@components/Cursor';
 import { RouterProvider } from 'react-router';
 import { GridBackground } from '@components/Background';
 
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <>
       {/* {鼠标效果} */}
-      <Cursor />
+      {/* <Cursor /> */}
       {/* {背景效果} */}
       <div className='fixed top-0 left-0 -z-1 h-full w-full'>
         <GridBackground direction='diagonal' speed={0.3} squareSize={35} borderColor='#eaeaea' />
